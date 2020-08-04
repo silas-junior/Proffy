53b1d3f77fa47b48690314ac8c048f7a6197978d
Silas Junior <silas.pereirajunior04@gmail.com>
2020-08-04 01:40:11 -0300

    add landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './assets/styles/global.css'
 
+import Landing from './pages/Landing/index'
+
 function App() {
   return (
     <div className="App">
-        <h1>Hello World!</h1>
+        <Landing />
     </div>
   );
 }
