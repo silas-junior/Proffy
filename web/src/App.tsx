93f5fd9b6ee9c07159b39cb33b7c72d5a84a7f44
Silas Junior <silas.pereirajunior04@gmail.com>
2020-08-05 18:25:06 -0300

    adding navigation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import Routes from './routes';
+
 import './assets/styles/global.css'
 
-import Landing from './pages/Landing/index'
 
 function App() {
   return (
     <div className="App">
-        <Landing />
+        <Routes />
     </div>
   );
 }
