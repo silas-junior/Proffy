13107495d4fb3701170c4d74c6d08f07e6644cb5
Silas Junior <silas.pereirajunior04@gmail.com>
2020-08-10 20:54:37 -0300

    TeacherList => updating component and styles

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from'react';
 
 import PageHeader from '../../components/PageHeader';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -29,32 +28,8 @@ function TeacherList() {
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://avatars0.githubusercontent.com/u/68646116?s=460&v=4" alt="Silas Pereira"/>
-                        <div>
-                            <strong>Silas Pereira</strong>
-                            <span>Arqueologia</span>
-                        </div>
-                    </header>
-
-                    <p>
-                        Apaixonado pelas melhores histórias e descobertas do passsado.
-                        <br></br>
-                         Fissurado em desenhos, fósseis, artefatos e lugares que remetem ao Período Cretáceo, Idade da Pedra, Egito e as Guerras Mundiais. São mais de 5 mil alunos que passaram pela "viagem no tempo" que eu proporciono.
-                    </p>
-
-                    <footer>
-                        <p>
-                            Preço/Hora
-                            <strong>R$ 50,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
+                <TeacherItem/>
+                <TeacherItem/>
             </main>
         </div>
     );
