fe62d190f02889b914b26e457172c98e66e3405e
Silas Junior <silas.pereirajunior04@gmail.com>
2020-08-19 19:38:40 -0300

    correct commit

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,8 +1,5 @@
 import express from 'express';
-<<<<<<< HEAD
-=======
 import cors from 'cors';
->>>>>>> f91fd2a76b862594acf07bf48e30681ee3cd920e
 import routes from './routes';
 
 const app = express();
@@ -11,8 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-<<<<<<< HEAD
-=======
 
->>>>>>> f91fd2a76b862594acf07bf48e30681ee3cd920e
 app.listen(3333);
